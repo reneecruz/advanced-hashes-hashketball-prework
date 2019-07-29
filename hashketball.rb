be8379d6bea3c7f9cb4172c5f78d5ad6c5bb9739
Renée Cruz <reneebasscruz@gmail.com>
2019-07-29 02:27:39 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,42 @@
-# Write your code here!
+def game_hash
+  game_hash = {
+    :home => {
+      :team_name => "",
+      :colors => %w{},
+      :players => [
+        {
+          :number =>
+          :shoe => 
+          :points =>
+          :rebounds =>
+          :assists =>
+          :steals =>
+          :blocks =>
+          :slam_dunks =>
+      }
+      ]
+      
+    }, 
+    :away => {
+      :team_name => "",
+      :colors => %w{}
+      :players => [
+        {
+          :number =>
+          :shoe => 
+          :points =>
+          :rebounds =>
+          :assists =>
+          :steals =>
+          :blocks =>
+          :slam_dunks =>
+        }
+        ]
+      
+    }
+    
+  }
+end
 
 
 
