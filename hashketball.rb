7bdbd60c3910a2bef005e8fd47b230868964a56a
Renée Cruz <reneebasscruz@gmail.com>
2019-07-29 02:46:56 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,25 +1,39 @@
 def game_hash
   game_hash = {
     :home => {
-      :team_name => "",
-      :colors => %w{},
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"]
       :players => [
-        {
-          :number =>
-          :shoe => 
-          :points =>
-          :rebounds =>
-          :assists =>
-          :steals =>
-          :blocks =>
-          :slam_dunks =>
-      }
+        {:player_name => "Alan Anderson"
+          {
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1
+          }
+        },
+         {:player_name => "Reggie Evans"
+          {
+          :number => 30,
+          :shoe => 14,
+          :points => 12,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 12,
+          :blocks => 12,
+          :slam_dunks => 7
+          }
+        }
       ]
       
     }, 
     :away => {
       :team_name => "",
-      :colors => %w{}
+      :colors => []
       :players => [
         {
           :number =>
